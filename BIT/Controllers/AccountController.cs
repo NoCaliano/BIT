25e59f4b24f156de1ab7e5833e831a1a371ac1f5
Nocaliano <you@example.com>
2023-11-24 21:49:23 +0200

    Мої замовлення

---
+++ BIT/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using BIT.DataStuff;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BIT.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult MyOrders() { return View(); }  
+
+    }
+}
