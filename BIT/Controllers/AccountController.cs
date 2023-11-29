53c4cb00a00005300d58d55456fc00963ba301bf
Nocaliano <you@example.com>
2023-11-29 23:17:14 +0200

    Добавлена можливість зміни пошти і паролю Account/Settings

--- BIT/Controllers/AccountController.cs
+++ BIT/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using BIT.DataStuff;
+﻿using BIT.Areas.Identity.Data;
+using BIT.DataStuff;
+using BIT.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -10,11 +13,14 @@ namespace BIT.Controllers
     {
         private readonly AppDbContext _context;
         private readonly ILogger<AccountController> _logger;
-
-        public AccountController(ILogger<AccountController> logger, AppDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly PasswordHasher<ApplicationUser> _passwordHasher;
+        public AccountController(ILogger<AccountController> logger, AppDbContext context, UserManager<ApplicationUser> userManager)
         {
+           
             _logger = logger;
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult MyOrders() { return View(); }
@@ -35,6 +41,80 @@ namespace BIT.Controllers
 
             return NotFound();
         }
+
+        public IActionResult Settings()
+        {
+           return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeEmail(string userId, string NewEmail)
+        {
+            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (loggedInUserId != userId)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var user = await _userManager.FindByIdAsync(userId);
+                var Token = await _userManager.GenerateChangeEmailTokenAsync(user, NewEmail);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                await _userManager.ChangeEmailAsync(user, NewEmail, Token);
+                await _userManager.SetUserNameAsync(user, NewEmail);
+                await _userManager.UpdateAsync(user);
+
+                return RedirectToAction("Settings", "Account");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(string userId, string OldPassword, string NewPassword)
+        {
+            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (loggedInUserId != userId)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var user = await _userManager.FindByIdAsync(userId);
+
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                var result = await _userManager.ChangePasswordAsync(user, OldPassword, NewPassword);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.UpdateAsync(user);
+                    return RedirectToAction("Settings", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    // Повертаємо перегляд разом із моделлю, щоб вивести повідомлення про помилки
+                    return View("Settings", new Settings
+                    {
+                        // Тут ви можете передати інші дані, які вам потрібні для відображення на сторінці
+                    });
+                }
+            }
+        }
+
+
 
 
     }
---
+++ BIT/Models/Settings.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BIT.Models
+{
+    // Моделька для інпутів у всякі форми
+    public class Settings
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Розширена реєстрація")]
+        public bool IsExtendedRegistration { get; set; }
+
+        [Required]
+        [Display(Name = "Ім'я")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Прізвище")]
+        public string LastName { get; set; }
+
+
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Телефон для зв'язку")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Місце проживання")]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name = "Вид транспорту")]
+        public string VehicleType { get; set; }
+    }
+}
